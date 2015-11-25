e5fb15779ba4a66cd022eed52e238dc0ead2a2f0
LukeMcGurdy <LukeMcGurdy@users.noreply.github.com>
2015-11-25 22:28:18 +0000

    latest base update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,73 +1,48 @@
 module.exports = function (grunt) {
 
-    // Project configuration.
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
         sass: {
-            // This will be executed when we run the 'development' task below
-            development: {
-                options: {
-                    style: 'expanded'
-                },
-                files: {
-                    'css/style.css': 'sass/style.scss'
-                }
+            options: {
+                sourceMap: false
             },
-            // This will be executed when we run the 'deploy' task below
-            deploy: {
-                options: {
-                    style: 'compressed'
-                },
+            dist: {
                 files: {
-                    'css/style.css': 'sass/style.scss'
+                    'dist/css/styles.css': 'sass/styles.scss'
                 }
             }
         },
+
         postcss: {
             options: {
                 map: {
-                    inline: false, // save all sourcemaps as separate files...
-                    annotation: 'css/maps/' // ...to the specified directory
+                    inline: false,
+                    annotation: 'dist/css/maps/'
                 },
                 processors: [
                     require('autoprefixer')({
                         browsers: 'last 2 versions'
-                    }), // add vendor prefixes
+                    }),
+                    require('cssnano')()
                 ]
             },
-            dist: {
-                src: 'css/*.css'
+            dist: {                
+                src: 'dist/css/styles.css'
             }
         },
+
         watch: {
             sass: {
-                files: ['**/*.scss'],
-                tasks: ['sass:dev']
+                files: 'sass/*.scss',
+                tasks: ['sass', 'postcss']
             },
-            postcss: {
-                files: 'css/style.css',
-                tasks: ['autoprefixer'],
-            },
-            livereload: {
-                options: {
-                    livereload: true
-                },
-                files: ['css/*.css', 'js/*.js', '*.html', 'img/*'],
-            }
         },
     });
 
-    // Load the Grunt plugins.
-    grunt.loadNpmTasks('grunt-postcss');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-livereload');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-cssnano');    
 
+    grunt.registerTask('default', ['watch']);
 
-    // Register the default tasks.
-    //    grunt.registerTask('default', ['watch']);
-    //    grunt.registerTask('development', ['sass:development']);
-    //    grunt.registerTask('deploy', ['sass:deploy']);
-
-    grunt.registerTask('default', ['postcss:dist', 'sass:dev', 'livereload', 'watch']);
 };
